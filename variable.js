a11692bbe23322b89a1761c322246a7b28cdac21
chandan kumar <ckrai9142@gmail.com>
2026-02-05 02:34:13 +0530

    variable

---
+++ variable.js
@@ -0,0 +1,17 @@
+const AccountId = 1234;
+let  AccountMail = "ckrai9142@gmail.com"
+// const use
+/*  const is fixed value keyword it can't change const new version of var
+*/
+accountCity = "Ara";
+accountstate = " Bihar";
+
+// let use
+/*  it is also  keyword 
+   let is flexiable ,. it can change every time
+*/
+AccountMail ="ck6296267@gmail.com";
+accountCity ="Patna"
+
+
+console.table([AccountId, AccountMail, accountCity, accountstate]);
